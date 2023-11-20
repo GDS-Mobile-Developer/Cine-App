6f57cec0d595df3b0fe98f03127a14655f30d6bf
Gds Desenvolvimento e Tecnologia <104175823+GabrielAlvesDoCarmo@users.noreply.github.com>
2023-11-20 02:54:37 -0300

    layout register screen

---
+++ app/src/main/java/com/gdsdevtec/cineapp/presenter/auth/register/RegisterFragment.kt
@@ -0,0 +1,19 @@
+package com.gdsdevtec.cineapp.presenter.auth.register
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.gdsdevtec.cineapp.databinding.FragmentRegisterBinding
+
+class RegisterFragment : Fragment() {
+    private lateinit var binding : FragmentRegisterBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ) = FragmentRegisterBinding.inflate(
+        inflater,container,false
+    ).root.also {
+        binding = FragmentRegisterBinding.bind(it)
+    }
+}
