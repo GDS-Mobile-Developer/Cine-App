6db84975fe38dc2a99213a411ca5796949eae27f
Gds Desenvolvimento e Tecnologia <104175823+GabrielAlvesDoCarmo@users.noreply.github.com>
2023-11-20 17:07:28 -0300

    included e config toolbar

--- app/src/main/java/com/gdsdevtec/cineapp/utils/Extensions.kt
+++ app/src/main/java/com/gdsdevtec/cineapp/utils/Extensions.kt
@@ -4,9 +4,12 @@ import android.content.Context
 import android.util.Patterns
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
+import com.gdsdevtec.cineapp.R
 
 fun Fragment.messageToast(msg: String) = Toast.makeText(
     requireContext(), msg, Toast.LENGTH_SHORT
@@ -34,10 +37,20 @@ fun String.isPasswordValid(): Boolean {
 fun Fragment.nextFragment(action : NavDirections){
     findNavController().navigate(action)
 }
-fun Fragment.popFragment(id : Int,inclusive : Boolean){
-    findNavController().popBackStack(id,inclusive)
-}
+fun Fragment.initToolbar(toolbar: Toolbar,showIconNavigation : Boolean = true){
+    (activity as AppCompatActivity).apply {
+        setSupportActionBar(toolbar)
+        title = ""
+    }
 
-fun Fragment.popFragment(){
-    findNavController().popBackStack()
+    if (showIconNavigation){
+        (activity as AppCompatActivity).supportActionBar?.apply {
+            setHomeAsUpIndicator(R.drawable.ic_back)
+            setDisplayHomeAsUpEnabled(true)
+        }
+    }
+    toolbar.setNavigationOnClickListener {
+        activity?.onBackPressedDispatcher?.onBackPressed()
+    }
+
 }
