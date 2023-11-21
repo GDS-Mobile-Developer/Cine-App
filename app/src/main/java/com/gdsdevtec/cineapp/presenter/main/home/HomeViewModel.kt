060506c2618ea46f29a84d1ab0130ef84fa4d4b5
Gds Desenvolvimento e Tecnologia <104175823+GabrielAlvesDoCarmo@users.noreply.github.com>
2023-11-21 01:29:07 -0300

    list genres

--- app/src/main/java/com/gdsdevtec/cineapp/presenter/main/home/HomeViewModel.kt
+++ app/src/main/java/com/gdsdevtec/cineapp/presenter/main/home/HomeViewModel.kt
@@ -1,6 +1,41 @@
 package com.gdsdevtec.cineapp.presenter.main.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.gdsdevtec.cineapp.BuildConfig
+import com.gdsdevtec.cineapp.data.mapper.toGenrePresentation
+import com.gdsdevtec.cineapp.domain.usecase.movie.GetGenresUseCase
+import com.gdsdevtec.cineapp.domain.usecase.movie.GetMoviesByGenreUseCase
+import com.gdsdevtec.cineapp.utils.StateView
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import retrofit2.HttpException
+import javax.inject.Inject
 
-class HomeViewModel : ViewModel() {
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val getGenresUseCase: GetGenresUseCase,
+    private val getMoviesByGenreUseCase: GetMoviesByGenreUseCase,
+) : ViewModel() {
+
+    fun getGenres() = liveData(Dispatchers.IO) {
+        try {
+            emit(StateView.Loading)
+            val genres = getGenresUseCase(
+                BuildConfig.API_KEY,
+                LANGUAGE_REQUEST
+            ).map { it.toGenrePresentation() }
+            emit(StateView.Success(data = genres, msg = null))
+        } catch (e: HttpException) {
+            e.printStackTrace()
+            e.message?.let { StateView.Error(it) }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            e.message?.let { StateView.Error(it) }
+        }
+    }
+
+    private companion object{
+        const val LANGUAGE_REQUEST = "pt-br"
+    }
 }
